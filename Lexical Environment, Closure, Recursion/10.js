ee5dd52fe41b512fd39d21c3d4b0a2a258a3c5c4
demianov-an <Alexm92@inbox.ru>
2024-01-13 16:46:19 +0600

    lesson 10

---
+++ Lexical Environment, Closure, Recursion/10.js
@@ -0,0 +1,22 @@
+// let car = 'bmw'
+
+// function startEngine() {
+//   console.log(car);
+// }
+
+// car = 'audi'
+
+// startEngine()
+
+// -----------------
+
+// example
+// globalLE = {
+//   environmentRecords : {
+//   }
+//   outer: null | LE
+// }
+// globalLE {} -> null
+
+//---------------------
+
