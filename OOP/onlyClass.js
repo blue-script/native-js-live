2b965fc595d4efe6ada71d94d7bf7e324fce4cc9
demianov-an <alexm92@inbox.ru>
2024-02-14 15:28:38 +0600

    наследование

--- OOP/onlyClass.js
+++ OOP/onlyClass.js
@@ -1,26 +1,28 @@
 class User {
-  #name = '' //- инкапсуляция
+  // #name = '' //- инкапсуляция
+  _name = '' //- инкапсуляция
   constructor(name, site, db) {
     // this._name = name - инкапсуляция
-    this.#name = name // - инкапсуляция
+    // this.#name = name // - инкапсуляция
+    this._name = name // - инкапсуляция
     this.site = site
     this.dateOfBirth = db
     this.counter = 0
   }
 
   get name() {
-    return this.#name + '!!!!!!'
+    return this._name + '!!!!!!'
   }
 
   set name(value) {
     if (value === 'Igor') throw new Error('this name is not allowed')
-    this.#name = value
+    this._name = value
   }
 
   hello() {
     debugger
     this.counter++
-    console.log(`I'm ${this.name} from ${this.site}`)
+    console.log(`I'm ${this._name} from ${this.site}`)
   }
 }
 
@@ -40,7 +42,29 @@ class Coder extends User {
   code() {
     console.log(`${this.name} my ${this.tech} code is: const sum = (a,b) => a+b`)
   }
+
+  hello() {
+    super.hello()
+    console.log('Go away')
+  }
+}
+
+class Hacker extends Coder {
+  constructor(a,b,c,d) {
+    super()
+    this.tech = 'XXX'
+    this.name = 'hacker'
+  }
+
+  code() {
+    console.log("I'll hack everything")
+  }
 }
 
 const c1 = new Coder('Great racoon', 'it-incubator.by', new Date(1998, 10, 11), 'c#')
 c1.code()
+c1.hello()
+
+const hacker = new Hacker('Great racoon', 'it-incubator.by', new Date(1998, 10, 11), 'c#')
+hacker.code()
+hacker.hello()
